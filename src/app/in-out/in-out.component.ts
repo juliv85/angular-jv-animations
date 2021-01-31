a47d59f1d2a7ecfe0231c8f854a19cef9656e4cc
juliv85 <juliv85@gmail.com>
2021-01-30 23:42:37 -0500

    2 files modified, 1 added

--- src/app/in-out/in-out.component.ts
+++ src/app/in-out/in-out.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  AnimationEvent
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-in-out',
-  templateUrl: './in-out.component.html',
-  styleUrls: ['./in-out.component.css']
+  selector: "app-in-out",
+  templateUrl: "./in-out.component.html",
+  styleUrls: ["./in-out.component.css"],
+  animations: [
+    trigger("inOut", [
+      //state("in", style({ transform: "translateX(0)" })),
+      transition(":enter", [
+        style({ transform: "translateX(-50%)" }),
+        animate(500)
+      ]),
+      transition(":leave", [
+        animate(500, style({ transform: "translateX(130%)" }))
+      ])
+    ])
+  ]
 })
 export class InOutComponent implements OnInit {
-
-  constructor() { }
+  allItems = ["item1", "item2", "item3"];
+  items = [];
+  elementRemoved = 0;
+  constructor() {}
 
   ngOnInit() {
+    setTimeout(() => (this.items = this.allItems), 1000);
   }
 
+  removeItems() {
+    this.items = this.items.splice(1);
+  }
+
+  done(event: AnimationEvent) {
+    if (event.toState === "void") {
+      this.elementRemoved++;
+    }
+  }
 }
