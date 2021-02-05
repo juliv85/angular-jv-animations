707a80b5e4b0616c11a9c63125408e83bec33c3f
juliv85 <juliv85@gmail.com>
2021-02-04 20:00:02 -0500

    2 files modified

--- src/app/query-stagger/query-stagger.component.ts
+++ src/app/query-stagger/query-stagger.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  trigger,
+  query,
+  stagger,
+  style,
+  transition,
+  animate,
+  AnimationEvent
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-query-stagger',
-  templateUrl: './query-stagger.component.html',
-  styleUrls: ['./query-stagger.component.css']
+  selector: "app-query-stagger",
+  templateUrl: "./query-stagger.component.html",
+  styleUrls: ["./query-stagger.component.css"],
+  animations: [
+    trigger("entering", [
+      //state("in", style({ transform: "translateX(0)" })),
+      transition(":enter", [
+        query(".item", [
+          style({ opacity: 0, transform: "translateY(-100px)" }),
+          stagger(-50, [animate(500, style({ opacity: 1, transform: "none" }))])
+        ])
+      ])
+    ])
+  ]
 })
 export class QueryStaggerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
